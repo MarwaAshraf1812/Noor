1f468487d76b011cb6bfebd4fd3aeb5a911a8173
MarwaAshraf1812 <marashraf090@gmail.com>
2026-05-20 00:35:48 +0300

    feat: implement Adhkar module with API data fetching, session logging, and gamified progress tracking

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import authRoute from "./modules/auth/auth.routes.js";
 import prayerRoutes from './modules/prayers/prayer.route.js';
 import quranRoutes from './modules/quran/quran.routes.js';
+import adhkarRoutes from './modules/adhkar/adhkar.route.js';
 
 const app = express();
 
@@ -20,6 +21,6 @@ app.use(cookieParser());
 app.use("/api/v1/auth", authRoute);
 app.use('/api/v1/prayer', prayerRoutes);
 app.use('/api/v1/quran', quranRoutes);
-
+app.use('/api/v1/adhkar', adhkarRoutes);
 
 export default app;
